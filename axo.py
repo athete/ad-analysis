fbedaa5896a4e5f49d6b53ed1b052ce85bb83641
athete <43140388+athete@users.noreply.github.com>
2025-02-10 21:46:41 +0000

    Apply black formatting

--- axo.py
+++ axo.py
@@ -1,11 +1,12 @@
 import awkward as ak
-import dask 
+import dask
 import dask_awkward as dak
 import hist
 import hist.dask as hda
 import json
 import numpy as np
 import vector
+
 vector.register_awkward()
 
 
@@ -16,24 +17,31 @@ from coffea.dataset_tools import apply_to_fileset, max_chunks, preprocess
 
 from utils.hist import create_1d_hist
 
+
 class AXOHistFactory(processor.ProcessorABC):
     def __init__(self, triggers, hists_to_process):
         object_cuts = {
-            'ScoutingPFJet': [('pt', 30.), ('eta', 3.0)],
-            'ScoutingElectron': [('pt', 10.), ('eta', 2.65)],
-            'ScoutingPhoton': [('pt', 10.), ('eta', 2.65)],
-            'ScoutingMuonVtx': [('pt', 3.), ('eta', 2.4)],
+            "ScoutingPFJet": [("pt", 30.0), ("eta", 3.0)],
+            "ScoutingElectron": [("pt", 10.0), ("eta", 2.65)],
+            "ScoutingPhoton": [("pt", 10.0), ("eta", 2.65)],
+            "ScoutingMuonVtx": [("pt", 3.0), ("eta", 2.4)],
         }
 
         self.triggers = triggers
         self.hists_to_process = hists_to_process
         self.hists = {}
 
-        self.trigger_axis = hist.axis.StrCategory([], name="trigger", label="Trigger", growth=True)
-        self.object_axis = hist.axis.StrCategory([], name="object", label="Object", growth=True)
+        self.trigger_axis = hist.axis.StrCategory(
+            [], name="trigger", label="Trigger", growth=True
+        )
+        self.object_axis = hist.axis.StrCategory(
+            [], name="object", label="Object", growth=True
+        )
         self.pt_axis = hist.axis.Regular(500, 0, 5000, name="pt", label=r"$p_T$ [GeV]")
         self.ht_axis = hist.axis.Regular(100, 0, 2000, name="ht", label=r"$H_T$ [GeV]")
-        self.met_axis = hist.axis.Regular(250, 0, 2500, name="met", label=r"$p^{\rm miss}_T$ [GeV]")
+        self.met_axis = hist.axis.Regular(
+            250, 0, 2500, name="met", label=r"$p^{\rm miss}_T$ [GeV]"
+        )
         self.eta_axis = hist.axis.Regular(150, -5, 5, name="eta", label=r"$\eta$")
         self.phi_axis = hist.axis.Regular(30, -4, 4, name="phi", label=r"$\phi$")
 
@@ -44,8 +52,14 @@ class AXOHistFactory(processor.ProcessorABC):
             {
                 "saturatedL1Jet": dak.all(events.L1Jet.pt < 1000, axis=1),
                 "saturatedL1MET": dak.all(
-                    dak.flatten(events.L1EtSum.pt[(events.L1EtSum.etSumType==2) & (events.L1EtSum.bx == 0)]) < 1040, axis=1
-                )
+                    dak.flatten(
+                        events.L1EtSum.pt[
+                            (events.L1EtSum.etSumType == 2) & (events.L1EtSum.bx == 0)
+                        ]
+                    )
+                    < 1040,
+                    axis=1,
+                ),
             }
         )
         saturation_cut = {"saturatedL1Jet", "saturatedL1MET"}
@@ -58,20 +72,30 @@ class AXOHistFactory(processor.ProcessorABC):
         if "ScoutingMET" in self.hists_to_process:
             self.hists["ScoutingMET"] = create_1d_hist(self.trigger_axis, self.met_axis)
         if "pt" in self.hists_to_process:
-            self.hists["pt"] = create_1d_hist(self.trigger_axis, self.pt_axis, self.object_axis)
+            self.hists["pt"] = create_1d_hist(
+                self.trigger_axis, self.pt_axis, self.object_axis
+            )
         if "LeadingPt" in self.hists_to_process:
-            self.hists["LeadingPt"] = create_1d_hist(self.trigger_axis, self.pt_axis, self.object_axis)
+            self.hists["LeadingPt"] = create_1d_hist(
+                self.trigger_axis, self.pt_axis, self.object_axis
+            )
         if "SubleadingPt" in self.hists_to_process:
-            self.hists["SubleadingPt"] = create_1d_hist(self.trigger_axis, self.pt_axis, self.object_axis)
+            self.hists["SubleadingPt"] = create_1d_hist(
+                self.trigger_axis, self.pt_axis, self.object_axis
+            )
         if "eta" in self.hists_to_process:
-            self.hists["eta"] = create_1d_hist(self.trigger_axis, self.eta_axis, self.object_axis)
+            self.hists["eta"] = create_1d_hist(
+                self.trigger_axis, self.eta_axis, self.object_axis
+            )
         if "phi" in self.hists_to_process:
-            self.hists["phi"] = create_1d_hist(self.trigger_axis, self.phi_axis, self.object_axis)
-        
+            self.hists["phi"] = create_1d_hist(
+                self.trigger_axis, self.phi_axis, self.object_axis
+            )
+
         # Apply triggers
         for trigger_path in self.triggers:
-            trigger_branch = getattr(events, trigger_path.split('_')[0])
-            trig_path = '_'.join(trigger_path.split('_')[1:])
+            trigger_branch = getattr(events, trigger_path.split("_")[0])
+            trig_path = "_".join(trigger_path.split("_")[1:])
             events_triggered = events[getattr(trigger_branch, trig_path)]
 
             if "ScoutingHT" in self.hists_to_process:
--- utils/hist.py
+++ utils/hist.py
@@ -1,4 +1,5 @@
 import hist.dask as hda
 
+
 def create_1d_hist(storage="weight", label="nEvents", **hist_axes) -> hda.Hist:
     return hda.hist.Hist(storage=storage, label=label, **hist_axes)
